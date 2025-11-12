9d3925b1e0539e735295876564ae2aeac5bcd958
denigor145 <denigor144@gmail.com>
2025-11-12 21:30:07 +0300

    Create opponents-data.js

---
+++ js/opponents-data.js
@@ -0,0 +1,29 @@
+// js/opponents-data.js
+const opponents = [
+    {
+        id: 1,
+        name: "Воин",
+        maxHealth: 100,
+        health: 100,
+        money: 150,
+        avatar: "🔴",
+        drops: [
+            { name: "трава", chance: 10 },
+            { name: "веревка", chance: 15 },
+            { name: "money", min: 25, max: 37, chance: 100 }
+        ]
+    },
+    {
+        id: 2,
+        name: "Маг", 
+        maxHealth: 120,
+        health: 120,
+        money: 200,
+        avatar: "🔵",
+        drops: [
+            { name: "вода", chance: 20 },
+            { name: "бумага", chance: 15 },
+            { name: "money", min: 30, max: 50, chance: 100 }
+        ]
+    }
+];
