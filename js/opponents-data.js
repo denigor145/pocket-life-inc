c524df252f71b3bb730eb0e6cc75f7fe24a53c99
denigor145 <denigor144@gmail.com>
2025-11-13 12:56:30 +0300

    Update opponents-data.js

--- js/opponents-data.js
+++ js/opponents-data.js
@@ -7,6 +7,7 @@ const opponents = [
         health: 100,
         money: 150,
         avatar: "👩‍💼",
+        background: "sales-pers1.jpg",
         drops: [
             { name: "трава", chance: 10 },
             { name: "веревка", chance: 15 },
@@ -20,10 +21,53 @@ const opponents = [
         health: 120,
         money: 200,
         avatar: "👨‍💼",
+        background: "sales-pers2.jpg",
         drops: [
             { name: "вода", chance: 20 },
             { name: "бумага", chance: 15 },
             { name: "money", min: 30, max: 50, chance: 100 }
+        ]
+    },
+    {
+        id: 3,
+        name: "Ирина Петрова",
+        maxHealth: 100,
+        health: 100,
+        money: 150,
+        avatar: "👩‍🎓",
+        background: "sales-pers3.jpg",
+        drops: [
+            { name: "трава", chance: 10 },
+            { name: "веревка", chance: 15 },
+            { name: "money", min: 25, max: 37, chance: 100 }
+        ]
+    },
+    {
+        id: 4,
+        name: "Дмитрий Волков",
+        maxHealth: 100,
+        health: 100,
+        money: 150,
+        avatar: "👨‍🔧",
+        background: "sales-pers4.jpg",
+        drops: [
+            { name: "трава", chance: 10 },
+            { name: "веревка", chance: 15 },
+            { name: "money", min: 25, max: 37, chance: 100 }
+        ]
+    },
+    {
+        id: 5,
+        name: "Ольга Семенова",
+        maxHealth: 100,
+        health: 100,
+        money: 150,
+        avatar: "👩‍🍳",
+        background: "sales-pers5.jpg",
+        drops: [
+            { name: "трава", chance: 10 },
+            { name: "веревка", chance: 15 },
+            { name: "money", min: 25, max: 37, chance: 100 }
         ]
     }
 ];
